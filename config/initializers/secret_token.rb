4bd485106f08fe8d67742079738b53d0df9b194d
Julian Miller <me@juliananthonymiller.com>
2011-10-18 20:29:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dongletap::Application.config.secret_token = 'f7027082c24191440da6bd105081a5bc10fb464b5945beb7f3028826d3cacdc9696c012b3af18d74657d168637ff2b51be0b46969974133ff94a9c7ed069d622'
