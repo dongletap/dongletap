06a15fb882f388cb9640e355b7d606b6fd74e7db
U-JimmyJangleDang\Dongus <Dongus@JimmyJangleDang.(none)>
2011-10-19 07:48:11 -0500

    test with new user name

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ Dongletap::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
